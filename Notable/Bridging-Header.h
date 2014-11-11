dde228f340fe6b9f333f964b1f09f64794daf1d1
Frida Samuelsson <fridasamuelsson@hotmail.com>
2014-11-11 09:37:47 +0100

    Revert "Facebook integration was made."
    This reverts commit 6c03176607ba6d1d4b1be9cbcb467753e01f762a.

--- Notable/Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Bridging-Header.h
-//  Notable
-//
-//  Created by abdullah cengiz on 10/11/14.
-//  Copyright (c) 2014 abdullah cengiz. All rights reserved.
-//
-
-#import <FacebookSDK/FacebookSDK.h>
